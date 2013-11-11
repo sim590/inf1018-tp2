6d1f6fd9c53cbb7a246bf58f146827bf36065673
Simon Désaulniers <rostydela@gmail.com>
2013-11-11 15:05:26 -0500

    ajout d'un Makefile

--- src/lexical.h
+++ src/lexical.h
@@ -1,4 +1,10 @@
+#ifndef LEXICAL_4UGTM7YE
+
+#define LEXICAL_4UGTM7YE
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void loadFile(char *filename);
+
+#endif /* end of include guard: LEXICAL_4UGTM7YE */
---
+++ src/syntaxique.h
@@ -0,0 +1,7 @@
+#ifndef SYNTAXIQUE_TUXL4CRQ
+
+#define SYNTAXIQUE_TUXL4CRQ
+
+
+
+#endif /* end of include guard: SYNTAXIQUE_TUXL4CRQ */
